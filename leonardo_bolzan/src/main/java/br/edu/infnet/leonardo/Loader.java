e7b1779a914816e7893c2178f214882639a7bb13
Leonardo Bolzan de Morais <leonardobolzandemorais@gmail.com>
2024-11-03 11:06:25 -0300

    Alteração na classe Loader para inicializar duas partidas

--- leonardo_bolzan/src/main/java/br/edu/infnet/leonardo/Loader.java
+++ leonardo_bolzan/src/main/java/br/edu/infnet/leonardo/Loader.java
@@ -1,6 +1,7 @@
 package br.edu.infnet.leonardo;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -8,7 +9,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import br.edu.infnet.leonardo.model.domain.Canal;
-import br.edu.infnet.leonardo.model.domain.Estatisticas;
 import br.edu.infnet.leonardo.model.domain.Jogador;
 import br.edu.infnet.leonardo.model.domain.Narrador;
 import br.edu.infnet.leonardo.model.domain.Partida;
@@ -24,36 +24,49 @@ public class Loader implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		
-		/// Define o time da casa
+		/// INT x GRE
+		Partida partida = GetPartida1();
+		partidaService.Add(partida);
+
+		/// INT x FLA
+		partida = GetPartida2();
+		partidaService.Add(partida);
+		
+	}
+	
+	private Partida GetPartida1() {
+
 		Time timeCasa = new Time("Internacional", "INT");
 		timeCasa.addJogador(new Jogador("Rochet", 1, "GOL"));
-		timeCasa.addJogador(new Jogador("Bruno Gomes", 2, "LD"));
-		timeCasa.addJogador(new Jogador("Bernabei", 6, "LE"));
-		timeCasa.addJogador(new Jogador("Vitão", 44, "ZAG"));
-		timeCasa.addJogador(new Jogador("Clayton Sampaio", 4, "ZAG"));
-		timeCasa.addJogador(new Jogador("Fernando", 5, "VOL"));
-		timeCasa.addJogador(new Jogador("Thiago Maia", 8, "VOL"));
-		timeCasa.addJogador(new Jogador("Alan Patrick", 10, "MC"));
-		timeCasa.addJogador(new Jogador("Wesley", 11, "ME"));
-		timeCasa.addJogador(new Jogador("Bruno Tabata", 7, "MD"));
-		timeCasa.addJogador(new Jogador("Borré", 9, "CA"));
-		
-		/// Define o time visitante
+		timeCasa.addJogador(new Jogador("Bruno Gomes", 15, "LAD"));
+		timeCasa.addJogador(new Jogador("Clayton Sampaio", 20, "ZAD"));
+		timeCasa.addJogador(new Jogador("Vitão", 44, "ZAE"));
+		timeCasa.addJogador(new Jogador("Bernabei", 26, "LAE"));
+		timeCasa.addJogador(new Jogador("Rômulo", 40, "VOL"));
+		timeCasa.addJogador(new Jogador("Bruno Henrique", 8, "VOL"));
+		timeCasa.addJogador(new Jogador("Alan Patrick", 10, "MEC"));
+		timeCasa.addJogador(new Jogador("Gabriel Carvalho", 34, "MEC"));
+		timeCasa.addJogador(new Jogador("Wesley", 21, "MEC"));
+		timeCasa.addJogador(new Jogador("Borré", 19, "ATA"));
+
 		Time timeVisitante = new Time("Grêmio", "GRE");
-		timeVisitante.addJogador(new Jogador("Machesin", 12, "GOL"));
-		timeVisitante.addJogador(new Jogador("João Pedro", 2, "LD"));
-		timeVisitante.addJogador(new Jogador("Reinaldo", 6, "LE"));
-		timeVisitante.addJogador(new Jogador("Rodrigo Ely", 3, "ZAG"));
-		timeVisitante.addJogador(new Jogador("Jemerson", 4, "ZAG"));
-		timeVisitante.addJogador(new Jogador("Dodi", 5, "VOL"));
-		timeVisitante.addJogador(new Jogador("Villasanti", 8, "VOL"));
-		timeVisitante.addJogador(new Jogador("Cristaldo", 10, "MC"));
-		timeVisitante.addJogador(new Jogador("Soteldo", 7, "ME"));
-		timeVisitante.addJogador(new Jogador("Aravena", 11, "MD"));
-		timeVisitante.addJogador(new Jogador("Braithwaite", 9, "CA"));
+		timeVisitante.addJogador(new Jogador("Machesin", 1, "GOL"));
+		timeVisitante.addJogador(new Jogador("João Pedro", 18, "LAD"));
+		timeVisitante.addJogador(new Jogador("Jemerson", 28, "ZAE"));
+		timeVisitante.addJogador(new Jogador("Rodrigo Ely", 5, "ZAD"));
+		timeVisitante.addJogador(new Jogador("Mayk", 26, "LAE"));
+		timeVisitante.addJogador(new Jogador("Dodi", 17, "VOL"));
+		timeVisitante.addJogador(new Jogador("Villasanti", 20, "VOL"));
+		timeVisitante.addJogador(new Jogador("Edenílson", 15, "VOL"));
+		timeVisitante.addJogador(new Jogador("Cristaldo", 10, "MEC"));
+		timeVisitante.addJogador(new Jogador("Aravena", 16, "MEC"));
+		timeVisitante.addJogador(new Jogador("Braithwaite", 22, "ATA"));
 		
 		/// Cria a partida
-		Partida partida = new Partida(timeCasa, timeVisitante, LocalDateTime.now(), "Estádio Beira-Rio");
+		Partida partida = new Partida(timeCasa,
+				timeVisitante,
+				GetLocalDateTime("2024-10-19 16:00"),
+				"Estádio Beira-Rio");
 
 		/// Associa um canal à partida
 		Narrador narrador = new Narrador("Paulo Brito", "Futebol do RS");
@@ -64,15 +77,54 @@ public class Loader implements ApplicationRunner {
 		narrador = new Narrador("Milton Leite", "Futebol nacional");
 		canal = new Canal("SporTV", 38, narrador);
 		partida.addCanal(canal);
-		
-		/// Inicializa as estatísticas do time da casa
-		var estatisticasTimeCasa = new Estatisticas();
-		partida.setEstatisticasTimeCasa(estatisticasTimeCasa);
 
-		/// Inicializa as estatísticas do time visitante
-		var estatisticasTimeVisitante = new Estatisticas();
-		partida.setEstatisticasTimeVisitante(estatisticasTimeVisitante);
+		return partida;
+	}
+	
+	private Partida GetPartida2() {
+
+		Time timeCasa = new Time("Internacional", "INT");
+		timeCasa.addJogador(new Jogador("Rochet", 1, "GOL"));
+		timeCasa.addJogador(new Jogador("Bruno Gomes", 15, "LAD"));
+		timeCasa.addJogador(new Jogador("Rogel", 18, "ZAD"));
+		timeCasa.addJogador(new Jogador("Vitão", 44, "ZAE"));
+		timeCasa.addJogador(new Jogador("Bernabei", 26, "LAE"));
+		timeCasa.addJogador(new Jogador("Rômulo", 40, "VOL"));
+		timeCasa.addJogador(new Jogador("Thiago Maia", 29, "VOL"));
+		timeCasa.addJogador(new Jogador("Alan Patrick", 10, "MEC"));
+		timeCasa.addJogador(new Jogador("Bruno Tabata", 27, "MEC"));
+		timeCasa.addJogador(new Jogador("Wesley", 21, "MEC"));
+		timeCasa.addJogador(new Jogador("Borré", 19, "ATA"));
 		
-		partidaService.Add(partida);
+		Time timeVisitante = new Time("Flamengo", "FLA");
+		timeVisitante.addJogador(new Jogador("Rossi", 1, "GOL"));
+		timeVisitante.addJogador(new Jogador("Varela", 2, "LAD"));
+		timeVisitante.addJogador(new Jogador("Fabrício Bruno", 15, "ZAD"));
+		timeVisitante.addJogador(new Jogador("Léo Ortiz", 3, "ZAE"));
+		timeVisitante.addJogador(new Jogador("Ayrton Lucas", 6, "LAE"));
+		timeVisitante.addJogador(new Jogador("Everton Araújo", 52, "VOL"));
+		timeVisitante.addJogador(new Jogador("Erick Pulgar", 5, "VOL"));
+		timeVisitante.addJogador(new Jogador("Alcaraz", 37, "MEC"));
+		timeVisitante.addJogador(new Jogador("Gerson", 8, "ATA"));
+		timeVisitante.addJogador(new Jogador("Plata", 45, "ATA"));
+		timeVisitante.addJogador(new Jogador("Bruno Henrique", 27, "ATA"));
+		
+		/// Cria a partida
+		Partida partida = new Partida(timeCasa,
+				timeVisitante,
+				GetLocalDateTime("2024-10-30 19:00"),
+				"Estádio Beira-Rio");
+
+		/// Associa um canal à partida
+		Narrador narrador = new Narrador("Paulo Brito", "Futebol do RS");
+		Canal canal = new Canal("Canal do Baldasso", 8, narrador);
+		partida.addCanal(canal);
+		
+		return partida;
+	}
+	
+	private static LocalDateTime GetLocalDateTime(String data) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		return LocalDateTime.parse(data, formatter);
 	}
 }
